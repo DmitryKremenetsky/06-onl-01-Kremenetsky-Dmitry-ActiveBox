97355dba1dea411881d5dcef60bd937765c23abf
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2021-01-31 20:01:12 +0300

    change gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ function htmlTask() {
 }
 
 function scssTask() {
-  return src('./styles/styles.scss')
+  return src('./styles/index.scss')
     .pipe(sourcemaps.init())
     .pipe(concat('styles.css'))
     .pipe(sass())
